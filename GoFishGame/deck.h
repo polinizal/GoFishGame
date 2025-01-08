3521465446a1c457f848e1c2d368288ef4946214
polinizal <polina.staneva@yahoo.com>
2025-01-09 00:02:28 +0200

    Creating Deck Structure

--- GoFishGame/deck.h
+++ GoFishGame/deck.h
@@ -1,12 +1,1 @@
 #pragma once
-#ifndef DECK_H
-#define DECK_H
-
-#include <vector>
-#include "card.h"
-
-std::vector<Card> createDeck();
-void shuffleDeck(std::vector<Card>& deck);
-Card dealCard(std::vector<Card>& deck);
-
-#endif
