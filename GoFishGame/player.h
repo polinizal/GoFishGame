951ba8ea67fd33d2227b2c67da16ad97ffd596fd
polinizal <polina.staneva@yahoo.com>
2025-01-14 23:53:46 +0200

    Add Player.h file with player related declarations of functions

---
+++ GoFishGame/player.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <vector>
+#include "card.h"
+#include "string"
+
+struct Player
+{
+	
+	std::vector<Card> hand;
+	std::vector<std::string> claimedSets;
+};
+
+void addCardToHand(Player& player, const Card& card);
+void removeCardsByRank(Player& player, const std::string& rank);
+bool hasFullSet(const Player& player, const std::string& rank);
+void claimSet(Player& player, const std::string& rank);
+void displayHand(const Player& player);
+
+#endif
