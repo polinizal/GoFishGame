3908a2e3d288042cdcf9b48f25dd875e9ccd4793
polinizal <polina.staneva@yahoo.com>
2025-01-16 22:57:09 +0200

    Add game.h file with game function declarations ;)

--- GoFishGame/game.h
+++ GoFishGame/game.h
@@ -1,1 +1,15 @@
 #pragma once
+#ifndef GAME_H
+#define GAME_H
+
+#include "player.h"
+#include "deck.h"
+
+// Function declarations
+void initializeGame(Player& player, Player& computer, Deck& deck);
+void handlePlayerTurn(Player& player, Player& computer, Deck& deck);
+void handleComputerTurn(Player& player, Player& computer, Deck& deck);
+bool isGameOver(const Player& player, const Player& computer, const Deck& deck);
+Player determineWinner(const Player& player, const Player& computer);
+
+#endif
