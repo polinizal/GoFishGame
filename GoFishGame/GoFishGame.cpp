21889676e48475c047e56e34288b87409335d39b
polinizal <polina.staneva@yahoo.com>
2025-01-18 20:41:07 +0200

    Updated main

--- GoFishGame/GoFishGame.cpp
+++ GoFishGame/GoFishGame.cpp
@@ -1,6 +1,7 @@
 ﻿#include "game.h"
 #include <iostream>
 
+
 int main() {
     // Create player and computer structures
     Player player;
@@ -11,31 +12,19 @@ int main() {
     initializeGame(player, computer, deck);
 
     // Main game loop for the first phase
-    while (areCardsEqualRank && !isGameOver(player, computer, deck)) {
-        // Player's turn
+    while ((!IsReadyForSecondPhase(deck,player,computer))&&(!isGameOver(player,computer))) {
+        
         handlePlayerTurn(player, computer, deck);
-
-        // Check if the deck is empty
-        if (!areThereCardsLeft) {
-            std::cout << "\nThe deck is now empty. Transitioning to the second phase of the game!" << std::endl;
-            break;
-        }
-
-        // Check if the game is over after the player's turn
-        if (isGameOver(player, computer, deck)) {
-            break;
-        }
-
-        // Computer's turn
+        
         handleComputerTurn(player, computer, deck);
     }
 
-    // Transition to the second phase if the game is not over
-    if (!isGameOver(player, computer, deck)) {
+    
+    if (!isGameOver(player, computer)) {
         handleSecondPhase(player, computer);
     }
 
-    // Determine and display the winner
+    
     determineWinner(player, computer);
 
     // End the game with a thank-you message
