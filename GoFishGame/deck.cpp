a3f061d85f659477c258e8acccbac9163cd4664b
polinizal <polina.staneva@yahoo.com>
2025-01-19 17:27:40 +0200

    Add style format deck.cpp

--- GoFishGame/deck.cpp
+++ GoFishGame/deck.cpp
@@ -5,64 +5,50 @@
 #include "card.h"
 
 Deck createDeck() {
+    Deck deck;
 
-	Deck deck;
+    for (const std::string& suit : SUITS) {
+        for (const std::string& rank : RANKS) {
+            deck.cards.push_back({ suit, rank });
+        }
+    }
 
-	for (const std::string& suit : SUITS) {
-
-		for (const std::string& rank : RANKS) {
-
-			deck.cards.push_back({ suit, rank });
-
-		}
-
-	}
-
-	return deck;
-
+    return deck;
 }
-
-
 
 void swapCards(Deck& deck, size_t i, size_t j) {
-
-	Card temp = deck.cards[i];
-	deck.cards[i] = deck.cards[j];
-	deck.cards[j] = temp;
+    Card temp = deck.cards[i];
+    deck.cards[i] = deck.cards[j];
+    deck.cards[j] = temp;
 }
 
-void shuffleDeck(Deck& deck)
-{
-	std::srand(std::time(0));
-	int n = deck.cards.size();
-	for (size_t i = 0; i < n; i++) {
-		int randomIndex = i + std::rand() % (n - i);
-		swapCards(deck, i, randomIndex);
-	}
-}
-Card drawCard(Deck& deck)
-{
-	if (deck.cards.empty())
-	{
-		throw std::runtime_error("Deck is empty! Cannot draw a card."); //see how this behaves
-	}
-	size_t randomIndex = std::rand() % deck.cards.size();
-	Card drawnCard = deck.cards[randomIndex];
-	swapCards(deck, randomIndex, deck.cards.size() - 1);
-	deck.cards.pop_back();
-	return drawnCard;
-
-}
-void dealCards(Deck& deck, std::vector<Card>& playerHand, std::vector<Card>& computerHand)
-{
-	for (int i = 0; i < 6; i++)
-	{
-		playerHand.push_back(drawCard(deck));
-		computerHand.push_back(drawCard(deck));
-	}
+void shuffleDeck(Deck& deck) {
+    std::srand(std::time(0));
+    int n = deck.cards.size();
+    for (size_t i = 0; i < n; i++) {
+        int randomIndex = i + std::rand() % (n - i);
+        swapCards(deck, i, randomIndex);
+    }
 }
 
-bool areThereCardsLeft(const Deck& deck)
-{
-	return !deck.cards.empty(); //deck.cards.empty() returns true if the deck is empty
+Card drawCard(Deck& deck) {
+    if (deck.cards.empty()) {
+        throw std::runtime_error("Deck is empty! Cannot draw a card.");
+    }
+    size_t randomIndex = std::rand() % deck.cards.size();
+    Card drawnCard = deck.cards[randomIndex];
+    swapCards(deck, randomIndex, deck.cards.size() - 1);
+    deck.cards.pop_back();
+    return drawnCard;
+}
+
+void dealCards(Deck& deck, std::vector<Card>& playerHand, std::vector<Card>& computerHand) {
+    for (int i = 0; i < 6; i++) {
+        playerHand.push_back(drawCard(deck));
+        computerHand.push_back(drawCard(deck));
+    }
+}
+
+bool areThereCardsLeft(const Deck& deck) {
+    return !deck.cards.empty();
 }
