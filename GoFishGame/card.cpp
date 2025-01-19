cd9b29f5c125249e587dfc0faff18cf4268551d2
polinizal <polina.staneva@yahoo.com>
2025-01-19 17:22:48 +0200

    Added Card styling

--- GoFishGame/card.cpp
+++ GoFishGame/card.cpp
@@ -2,22 +2,22 @@
 
 std::string cardToString(const Card& card)
 {
-	return card.rank + "of" + card.suit;
+    return card.rank + " of " + card.suit;
 }
 
 bool areCardsEqualRank(const Card& card1, const Card& card2)
 {
-	return card1.rank == card2.rank;
+    return card1.rank == card2.rank;
 }
 
 bool isValidRank(const std::string& rank)
 {
-	for (const std::string& validRank : RANKS)
-	{
-		if (validRank == rank)
-		{
-			return true;
-		}
-	}
-	return false;
+    for (const std::string& validRank : RANKS)
+    {
+        if (validRank == rank)
+        {
+            return true;
+        }
+    }
+    return false;
 }
--- GoFishGame/card.h
+++ GoFishGame/card.h
@@ -5,15 +5,14 @@
 #include <vector>
 
 const std::vector<std::string> SUITS = { "Hearts", "Diamonds", "Clubs", "Spades" };
-const std::vector<std::string> RANKS  = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
-//Define a struct Card - a card has a suit and a rank
+const std::vector<std::string> RANKS = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
 struct Card
 {
-	std::string suit;
-	std::string rank;
+    std::string suit;
+    std::string rank;
 };
 
-//Functions 
 std::string cardToString(const Card& card);
 bool areCardsEqualRank(const Card& card1, const Card& card2);
 bool isValidRank(const std::string& rank);
